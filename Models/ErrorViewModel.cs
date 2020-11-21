beb0896ff843657b6c21933a444d6de53d7adefb
Edwart Sanchez <machiavelick22@gmail.com>
2020-11-20 20:00:19 -0500

    Formato MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetLover.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
